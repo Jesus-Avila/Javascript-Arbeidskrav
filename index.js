82d73221bf639f4a6c7548d3b6f9a145af26b779
lotteelisec <lotteelisec@gmail.com>
2024-04-06 17:12:09 +0200

    Opprettet element/hjerteikon i funksjonen charactercard og la til styling for ikonet

--- index.js
+++ index.js
@@ -12,28 +12,40 @@ const fetchData = () => {
       })
       .catch((error) => console.error("Error fetching data:", error));
   };
+
+  fetchData();
+
   
   const characterCard = (data) => {
     const characterListDiv = document.getElementById("characterContainer");
     data.forEach((character) => {
-      const name = character.name;
       const image = character.imageUrl;
-  
+      //Div
       const characterDiv = document.createElement("div");
       characterDiv.classList.add("character");
-  
+      //navn
       const nameElement = document.createElement("p");
-      nameElement.textContent = `Name: ${name}`;
-  
+      nameElement.textContent = `Name: ${character.name}`;
+      //bilde
       const imageElement = document.createElement("img");
       imageElement.src = image;
-      imageElement.alt = name;
-  
+      imageElement.alt = character.name;
+      //Hjerteikon
+      const heartIcon = document.createElement("i");
+      heartIcon.classList.add("fa", "fa-heart", "heartIcon");
+      heartIcon.style.fontSize = "36px";
+      heartIcon.style.opacity = "50%";
+      heartIcon.style.position = "absolute";
+      heartIcon.style.top = "0";
+      heartIcon.style.right = "0";
+      heartIcon.style.zIndex = "999"
+      heartIcon.style.margin = "20px"
+
+      //Legge til elementene
       characterDiv.appendChild(nameElement);
       characterDiv.appendChild(imageElement);
-  
+      characterDiv.appendChild(heartIcon);
       characterListDiv.appendChild(characterDiv);
     });
   };
   
-  fetchData();
