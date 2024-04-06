a949e47b8cf0c67ae240bc23ce879c7605df5693
Jesus Avila <jesus.shark27@hotmail.com>
2024-04-06 22:12:47 +0200

    bedre struktur på info.js

--- JS/info.js
+++ JS/info.js
@@ -6,7 +6,7 @@ const cosmeticID = 'Backpack_BrakePedal'
 const namePlaceholder = document.querySelector('#cosmeticName');
 const cosmeticImageContainer = document.querySelector('#cosmeticImageContainer');
 const cosmeticDescription = document.querySelector('#cosmeticDescription');
-const coseticRarity = document.querySelector('#cosmeticRarity');
+const cosmeticRarity = document.querySelector('#cosmeticRarity');
 
 
 async function fetchCosmetic() {
@@ -16,29 +16,33 @@ async function fetchCosmetic() {
             throw new Error('Network response was not ok');
         }
         const data = await response.json();
-        console.log(data);
-
-
-        namePlaceholder.innerHTML = data.data.name;
-
-        coseticRarity.innerHTML = data.data.rarity.displayValue;
-
-        cosmeticDescription.innerHTML = data.data.description;
-
-        const cosmeticImage = document.createElement('img');
-        cosmeticImage.src = data.data.images.icon;
-        cosmeticImage.style.width = '100%';
-        cosmeticImage.style.overflow = 'hidden';
-        cosmeticImageContainer.append(cosmeticImage);
-
-
+        console.log('from fetchcosm', data);
         return data;
     } catch (error) {
         console.error('Error:', error);
     }    
-    console.log(data.data.name);
-    
 }
 
+function displayCosmetic(data) {
+    // Create and display of image
+    const cosmeticImage = document.createElement('img');
+    cosmeticImage.src = data.data.images.icon;
+    cosmeticImage.style.width = '100%';
+    cosmeticImage.style.overflow = 'hidden';
+    cosmeticImageContainer.append(cosmeticImage);
 
-fetchCosmetic();
+    // Display name of cosmetic
+    namePlaceholder.innerHTML = data.data.name;
+
+    // Display rarity of cosmetic
+    cosmeticRarity.innerHTML = data.data.rarity.displayValue;
+}
+
+async function fetchAndDisplayCosmetic() {
+    try {
+        const data = await fetchCosmetic();
+        displayCosmetic(data);
+    } catch (error) {
+        console.error('Error:', error);
+    }
+}
