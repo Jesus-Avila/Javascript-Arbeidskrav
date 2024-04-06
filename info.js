5b11bf99c07ed1b56c15700d6b3e6a51c66c7584
Jesus Avila <jesus.shark27@hotmail.com>
2024-04-06 12:00:51 +0200

    fortsette med info.js

--- info.js
+++ info.js
@@ -2,25 +2,35 @@ baseUrl = "https://api.disneyapi.dev/character";
 
 var container = document.getElementById('container-name');
 
-async function fetchData() {
+const info = async function fetchData() {
     try {
-        const response = await fetch(baseUrl + '?name=Mickey%20Mouse')
+        const response = await fetch(baseUrl + '/4703')
             if (!response.ok) {
                 throw new Error('Network response was not ok');
             }
             const data = await response.json();
             console.log('clean data', data);
-            console.log('filtered data', data.data[1].name);
-            console.log('filtered data', data.data[1].films);
+            console.log('id of character', data.data[1]._id);
             container.innerHTML = data.data[1].name;
 
+            // Create a new <pre> element
+            const pre = document.createElement('pre');
+            // Sets its content
+            pre.innerText = JSON.stringify(data, null, 2);
+            // Appends it to the container
+            container.appendChild(pre);
+            
+            return data;
         } 
             catch(error) {
             console.error('There was a problem with the fetch operation:', error);
         };
 }
-
 fetchData();
+
+
+
+
 
 // var infoContainer = document.getElementById('info-container');
 
