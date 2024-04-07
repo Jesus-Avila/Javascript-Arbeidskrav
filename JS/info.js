2d2ea2879606b8e1a3358846998face73e6b6741
Jesus Avila <jesus.shark27@hotmail.com>
2024-04-07 17:53:10 +0200

    endre farge funksjon på info kortet

--- JS/info.js
+++ JS/info.js
@@ -6,6 +6,7 @@ const namePlaceholder = document.querySelector('#cosmeticName');
 const cosmeticImageContainer = document.querySelector('#cosmeticImageContainer');
 const cosmeticDescription = document.querySelector('#cosmeticDescription');
 const cosmeticRarity = document.querySelector('#cosmeticRarity');
+const comseticInfoCard = document.querySelector('#cosmeticInfoCard');
 
 
 async function fetchCosmetic(newCosmeticID) {
@@ -16,6 +17,7 @@ async function fetchCosmetic(newCosmeticID) {
         }
         const data = await response.json();
         console.log('from fetchcosm', data);
+        console.log('for color change', data.data.rarity.value.toLowerCase());
         return data;
     } catch (error) {
         console.error('Error:', error);
@@ -38,8 +40,10 @@ function displayCosmetic(data) {
 
     // Display description of cosmetic
     cosmeticDescription.innerHTML = data.data.description;
-}
 
+    // Change background color of cosmetic card based on rarity
+    changeCardColor(data);
+}
 
 async function fetchAndDisplayCosmetic(newCosmeticID) {
     try {
@@ -59,14 +63,40 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchAndDisplayCosmetic(newCosmeticID);
 })
 
-
 // Tilbake pil på info-siden
 const backToHomeArrow = document.querySelector("#chevronTextContainer");
-// backToHomeArrow.style.cursor = "pointer";
-
 backToHomeArrow.addEventListener("click", () => {
     window.location.href = "index.html";
 });
+
+    // Endre bakgrunnsfarge basert på rarity
+function changeCardColor (data) {
+    switch (data.data.rarity.value.toLowerCase()) {
+      case "uncommon":
+        comseticInfoCard.style.backgroundColor = "green";
+        break;
+      case "common":
+        comseticInfoCard.style.backgroundColor = "grey";
+        break;
+      case "rare":
+        comseticInfoCard.style.backgroundColor = "blue";
+        break;
+      case "epic":
+        comseticInfoCard.style.backgroundColor = "purple";
+        break;
+      case "legendary":
+        comseticInfoCard.style.backgroundColor = "orange";
+        break;
+      case "mythic":
+        comseticInfoCard.style.backgroundColor = "yellow";
+        break;
+      case "exotic":
+        comseticInfoCard.style.backgroundColor = "turquoise";
+        break;
+      default:
+        comseticInfoCard.style.backgroundColor = "#20263d"; // Default color
+    }
+}
 
 // Eventlistener og kaller på funksjonen addFavourite
 const addFavouriteButton = document.querySelector('#addToFavoritesBtn');
