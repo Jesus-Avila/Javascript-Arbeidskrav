b6a61d996271d25e73402c97da2b2de1cd2f3c96
lotteelisec <lotteelisec@gmail.com>
2024-04-10 17:47:11 +0200

    Fjernet en funksjon som tilhørte en seksjon som er slettet

--- JS/favourites.js
+++ JS/favourites.js
@@ -10,21 +10,3 @@ const clearFavorites = () => {
 //knapp som kaller på funksjonen
 const clearBtn = document.getElementById("clearBtn");
 clearBtn.addEventListener("click", clearFavorites);
-
-
-//Funksjon som oppretter listitems
-const addSuggestion = (text) => {
-  const list = document.getElementById("list");
-  const listItem = document.createElement("li");
-  listItem.textContent = text;
-  list.appendChild(listItem);
-};
-
-const searchField = document.getElementById("review"); 
-searchField.addEventListener("keydown", (event) => {
-  if (event.key === "Enter") {
-    const inputText = searchField.value; 
-    addSuggestion(inputText);
-    searchField.value = ""; 
-  }
-});
