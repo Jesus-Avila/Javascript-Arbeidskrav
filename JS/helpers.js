6d3521b93bd66365981fc29d8c65697d5317834c
lotteelisec <lotteelisec@gmail.com>
2024-04-11 08:56:21 +0200

    Endret en skrivefeil

--- JS/helpers.js
+++ JS/helpers.js
@@ -139,7 +139,7 @@ export const characterCard = (character) => {
 //funksjon som sender favoritt til localstorage og poster til crudcrud når man trykker på hjertet
 const urlPost = "https://crudcrud.com/api/2f342e16b33a4addbdea72e80bf1ffb0/resource";
 export const addFavouriteCrud = async (character) => {
-  let favoriteList = JSON.parse(localStorage.getItem("favorittList")) || [];
+  let favoriteList = JSON.parse(localStorage.getItem("favoriteList")) || [];
   favoriteList.push(character);
   localStorage.setItem("favoriteList", JSON.stringify(favoriteList));
   try {
