3c3d31c0027e2543ddbdbf8fa50c4355fb7bbe26
lotteelisec <lotteelisec@gmail.com>
2024-04-13 17:09:01 +0200

    Justert på lightmode på index

--- JS/mode.js
+++ JS/mode.js
@@ -1,12 +1,19 @@
 const darkModeToggle = document.getElementById('dark-mode-toggle');
 const body = document.body;
+const nav = document.querySelector('nav');
 
 darkModeToggle.addEventListener('click', () => {
   if (body.classList.contains('dark-mode')) {
     body.classList.remove('dark-mode');
+    nav.classList.remove('dark-mode');
+    body.classList.add('light-mode');
+    nav.classList.add('light-mode'); 
     localStorage.setItem('mode', 'light');
   } else {
+    body.classList.remove('light-mode');
     body.classList.add('dark-mode');
+    nav.classList.add('dark-mode');
+    nav.classList.remove('light-mode');
     localStorage.setItem('mode', 'dark');
   }
 });
